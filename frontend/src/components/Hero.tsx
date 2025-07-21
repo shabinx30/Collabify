9cc99071bdbb9a1db24507394bd5f7e10f3af559
shabinx30 <shabeensharih@gmail.com>
2025-07-22 01:00:46 +0530

    style: responsiveness fixed

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -4,15 +4,15 @@ import { LiaTelegramPlane } from "react-icons/lia";
 
 const Hero = () => {
     return (
-        <div className="w-full bg-white flex flex-col items-center px-[10em] pt-[4em]">
-            <h1 className="text-black text-5xl font-semibold">
+        <div className="w-full bg-white flex flex-col items-center px-[2em] lg:px-[10em] pt-[4em]">
+            <h1 className="w-full text-black text-3xl md:text-4xl lg:text-5xl font-semibold text-center">
                 Symplifiying Influencer Marketing For Everyone
             </h1>
-            <p className="text-gray-800 mt-[1em] w-[50%] text-center leading-[1.2]">
+            <p className="text-gray-800 mt-[1em] w-[75%] text-sm md:text-base lg:w-[50%] text-center leading-[1.2]">
                 Discover a smarter way to connect with content creators, launch
                 campaigns, and grow your, all in one simple, seamless platform.
             </p>
-            <div className="bg-[#FFF5CF] w-full flex p-2 rounded-2xl my-[4em] text-black">
+            <div className="bg-[#fff4c9] w-full md:flex p-2 rounded-2xl my-[4em] text-black">
                 <div className="flex-1/4">
                     <img
                         className="saturate-[130%] rounded-2xl"
@@ -22,7 +22,7 @@ const Hero = () => {
                 </div>
                 <div className="flex-1 pl-4 pr-2 py-2">
                     <div className="flex flex-col h-full justify-between">
-                        <div className="flex gap-4">
+                        <div className="flex gap-4 mb-[2em] md:mb-0">
                             <div className="flex-1 flex gap-4 justify-center items-center bg-[#F4E1FF] text-[#7B00E0] h-[3em] rounded-2xl">
                                 <FaInstagram size={22}/>
                                 Instagram
@@ -33,7 +33,7 @@ const Hero = () => {
                             </div>
                         </div>
                         <div className="flex flex-col gap-4">
-                            <div className="flex gap-14 justify-center text-lg font-semibold">
+                            <div className="flex gap-14 justify-center lg:text-lg font-semibold">
                                 <h2>Jennifer</h2>
                                 <div>
                                     <h2>Followers</h2>
@@ -54,7 +54,7 @@ const Hero = () => {
                                 <h2>⭐5.0</h2>
                             </div>
                         </div>
-                        <div className="flex gap-4">
+                        <div className="flex gap-4 mt-[2em] md:mt-0">
                             <div className="flex-1 flex gap-2 justify-center items-center bg-[#FFBF00] font-semibold h-[3em] rounded-2xl">
                                 <VscEye size={24} />
                                 View Profile
