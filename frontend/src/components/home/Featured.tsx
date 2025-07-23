e82d906b538ca3dde0b03e028f224507e776be49
shabinx30 <shabeensharih@gmail.com>
2025-07-23 20:56:21 +0530

    feat(Featured): enhance featured section with detailed item layout

--- frontend/src/components/home/Featured.tsx
+++ frontend/src/components/home/Featured.tsx
@@ -3,10 +3,54 @@ const Featured = () => {
         <section className="px-[2em] lg:px-[6em] xl:px-[12em] bg-white text-black">
             <h1 className="font-semibold text-xl">Featured</h1>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 py-[1em]">
-                <div className="aspect-square bg-gray-300 rounded-xl"></div>
-                <div className="aspect-square bg-gray-300 rounded-xl"></div>
-                <div className="aspect-square bg-gray-300 rounded-xl"></div>
-                <div className="aspect-square bg-gray-300 rounded-xl"></div>
+                <div className="aspect-[9/11]">
+                    <div className="bg-gray-300 h-[84%] w-full rounded-xl">
+                        {/* image */}
+                    </div>
+                    <div className="flex justify-between text-[0.9rem]">
+                        <div>
+                            <p>Beauty marketer & Content...</p>
+                            <p className="text-gray-600">Los Angeles, US</p>
+                        </div>
+                        <strong>$100</strong>
+                    </div>
+                </div>
+                <div className="aspect-[9/11]">
+                    <div className="bg-gray-300 h-[84%] w-full rounded-xl">
+                        {/* image */}
+                    </div>
+                    <div className="flex justify-between text-[0.9rem]">
+                        <div>
+                            <p>Beauty marketer & Content...</p>
+                            <p className="text-gray-600">Los Angeles, US</p>
+                        </div>
+                        <strong>$100</strong>
+                    </div>
+                </div>
+                <div className="aspect-[9/11]">
+                    <div className="bg-gray-300 h-[84%] w-full rounded-xl">
+                        {/* image */}
+                    </div>
+                    <div className="flex justify-between text-[0.9rem]">
+                        <div>
+                            <p>Beauty marketer & Content...</p>
+                            <p className="text-gray-600">Los Angeles, US</p>
+                        </div>
+                        <strong>$100</strong>
+                    </div>
+                </div>
+                <div className="aspect-[9/11]">
+                    <div className="bg-gray-300 h-[84%] w-full rounded-xl">
+                        {/* image */}
+                    </div>
+                    <div className="flex justify-between text-[0.9rem]">
+                        <div>
+                            <p>Beauty marketer & Content...</p>
+                            <p className="text-gray-600">Los Angeles, US</p>
+                        </div>
+                        <strong>$100</strong>
+                    </div>
+                </div>
             </div>
         </section>
     );
