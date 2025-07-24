d9757b60f54d0780c276a6c1ca01fe8ea07f0dbb
shabinx30 <shabeensharih@gmail.com>
2025-07-24 16:22:26 +0530

    feat: signup page has been added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login"
+import SignUp from "./pages/SignUp";
 
 function App() {
     return (
-        <>
-            {/* <Home /> */}
-            <Login />
-        </>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+        </Routes>
     );
 }
 
