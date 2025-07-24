6dfb45600ab7228406a43d80f23b0b298db49c97
shabinx30 <shabeensharih@gmail.com>
2025-07-24 16:27:23 +0530

    feat(Navbar): Links added

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,16 +1,24 @@
 import { FiSearch } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-const NavBar = ({page}: {page: 'login' | 'signup'}) => {
+const NavBar = ({ page }: { page: "login" | "signup" }) => {
     return (
-        <nav className="bg-white text-black">
+        <nav className="bg-white dark:bg-[#1b1b1b]">
             <ul className="flex py-2 justify-between px-4">
-                <li className="flex-1 font-bold my-1 text-lg md:text-2xl">Logo</li>
+                <li className="flex-1 font-bold my-1 text-lg md:text-2xl">
+                    {" "}
+                    <Link to="/">Logo</Link>{" "}
+                </li>
                 <li className="hidden md:flex flex-1 items-center px-4 gap-2 border border-[#868686] rounded-xl">
                     <FiSearch />
-                    <input type="text" className="w-full h-full outline-none" placeholder="Search"/>
+                    <input
+                        type="text"
+                        className="w-full h-full outline-none"
+                        placeholder="Search"
+                    />
                 </li>
                 <li className="flex-1/3 lg:flex-1 flex text-sm lg:text-base justify-end gap-6 my-1">
-                    {page != "login" && <p>Login</p>}
+                    {page != "login" && <Link to="/login">Login</Link>}
                     <p>Join as Brand</p>
                     <p className="text-[#FFBF00]">Join as Creator</p>
                 </li>
