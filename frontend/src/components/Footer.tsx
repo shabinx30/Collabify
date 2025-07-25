b8f70fa41a2f2970457de30dd25dcac5e20125d4
shabinx30 <shabeensharih@gmail.com>
2025-07-25 21:10:03 +0530

    style(Footer): improve layout and spacing for better readability

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,15 +2,36 @@ import { FaInstagram, FaXTwitter } from "react-icons/fa6";
 import { LuFacebook } from "react-icons/lu";
 
 const Footer = () => {
-    return <footer className="bg-[#202020] dark:bg-black p-4 flex justify-between items-center">
-        <h3 className="text-xl text-white">Logo</h3>
-        <p className="text-sm text-gray-400">All right reserved &copy; 2025</p>
-        <div className="flex gap-2">
-            <a target="_blank" rel="noopener noreferrer"><FaInstagram size={24} className="bg-white text-black p-1 rounded-full"/></a>
-            <a target="_blank" rel="noopener noreferrer"><FaXTwitter size={24} className="bg-white text-black p-1 rounded-full"/></a>
-            <a target="_blank" rel="noopener noreferrer"><LuFacebook size={24} className="bg-white text-black p-1 rounded-full"/></a>
-        </div>
-    </footer>
+    return (
+        <footer className="bg-[#202020] dark:bg-black p-4">
+            <h3 className="text-xl text-white my-[2em]">Logo</h3>
+            <div className="flex justify-between items-center">
+                <p className="text-sm text-gray-400">
+                    All right reserved &copy; 2025
+                </p>
+                <div className="flex gap-2">
+                    <a target="_blank" rel="noopener noreferrer">
+                        <FaInstagram
+                            size={24}
+                            className="bg-white text-black p-1 rounded-full"
+                        />
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer">
+                        <FaXTwitter
+                            size={24}
+                            className="bg-white text-black p-1 rounded-full"
+                        />
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer">
+                        <LuFacebook
+                            size={24}
+                            className="bg-white text-black p-1 rounded-full"
+                        />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
