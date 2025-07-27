a09323be5bbc823aa788afe63fc853c1a38588a5
shabinx30 <shabeensharih@gmail.com>
2025-07-27 23:06:51 +0530

    feat(Featured): replace img with next/image for better performance

--- frontend/src/components/home/Featured.tsx
+++ frontend/src/components/home/Featured.tsx
@@ -1,52 +1,82 @@
+import Image from "next/image";
+
 const Featured = () => {
     return (
         <section className="bg-white dark:bg-[#1b1b1b]">
             <h1 className="font-semibold text-xl">Featured</h1>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 py-[1em]">
                 <div className="aspect-[9/14]">
-                    <div className="h-[84%] w-full">
-                        <img className="object-cover w-full h-full rounded-xl" src="/images/infu-1.jpeg" alt="infu" />
+                    <div className="relative h-[84%] w-full">
+                        <Image
+                            className="object-cover w-full h-full rounded-xl"
+                            src="/images/infu-1.jpeg"
+                            alt="infu"
+                            fill
+                        />
                     </div>
                     <div className="flex justify-between text-[0.9rem]">
                         <div>
                             <p>Beauty marketer & Content...</p>
-                            <p className="text-gray-600 dark:text-gray-400">Los Angeles, US</p>
+                            <p className="text-gray-600 dark:text-gray-400">
+                                Los Angeles, US
+                            </p>
                         </div>
                         <strong className="text-base">$100</strong>
                     </div>
                 </div>
                 <div className="aspect-[9/14]">
-                    <div className="h-[84%] w-full">
-                        <img className="object-cover w-full h-full rounded-xl" src="/images/infu-2.jpeg" alt="infu" />
+                    <div className="relative h-[84%] w-full">
+                        <Image
+                            className="object-cover w-full h-full rounded-xl"
+                            src="/images/infu-2.jpeg"
+                            alt="infu"
+                            fill
+                        />
                     </div>
                     <div className="flex justify-between text-[0.9rem]">
                         <div>
                             <p>Beauty marketer & Content...</p>
-                            <p className="text-gray-600 dark:text-gray-400">Los Angeles, US</p>
+                            <p className="text-gray-600 dark:text-gray-400">
+                                Los Angeles, US
+                            </p>
                         </div>
                         <strong className="text-base">$100</strong>
                     </div>
                 </div>
                 <div className="aspect-[9/14]">
-                    <div className="h-[84%] w-full">
-                        <img className="object-cover w-full h-full rounded-xl" src="/images/infu-3.jpeg" alt="infu" />
+                    <div className="relative h-[84%] w-full">
+                        <Image
+                            className="object-cover w-full h-full rounded-xl"
+                            src="/images/infu-3.jpeg"
+                            alt="infu"
+                            fill
+                        />
                     </div>
                     <div className="flex justify-between text-[0.9rem]">
                         <div>
                             <p>Beauty marketer & Content...</p>
-                            <p className="text-gray-600 dark:text-gray-400">Los Angeles, US</p>
+                            <p className="text-gray-600 dark:text-gray-400">
+                                Los Angeles, US
+                            </p>
                         </div>
                         <strong className="text-base">$100</strong>
                     </div>
                 </div>
                 <div className="aspect-[9/14]">
-                    <div className="h-[84%] w-full ">
-                        <img className="object-cover w-full h-full rounded-xl" src="/images/infu-4.jpeg" alt="infu" />
+                    <div className="relative h-[84%] w-full ">
+                        <Image
+                            className="object-cover w-full h-full rounded-xl"
+                            src="/images/infu-4.jpeg"
+                            alt="infu"
+                            fill
+                        />
                     </div>
                     <div className="flex justify-between text-[0.9rem]">
                         <div>
                             <p>Beauty marketer & Content...</p>
-                            <p className="text-gray-600 dark:text-gray-400">Los Angeles, US</p>
+                            <p className="text-gray-600 dark:text-gray-400">
+                                Los Angeles, US
+                            </p>
                         </div>
                         <strong className="text-base">$100</strong>
                     </div>
