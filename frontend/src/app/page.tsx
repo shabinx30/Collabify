a6e72d6d95dc6a8b8921c15e7c3d3fe27994464e
shabinx30 <shabeensharih@gmail.com>
2025-07-29 18:55:52 +0530

    refactor(footer): now mounting on main layout.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,14 @@
-import Footer from "@/components/Footer";
 import Benefits from "@/components/home/Benefits";
 import Featured from "@/components/home/Featured";
 import Hero from "@/components/home/Hero";
-import NavBar from "@/components/NavBar";
 
 const Home = () => {
     return (
-        <>
-            <main className="flex flex-col gap-[4em] py-[6em] bg-white dark:bg-[#1b1b1b] px-[2em] lg:px-[6em] xl:px-[12em]">
-                <Hero />
-                <Featured />
-                <Benefits />
-            </main>
-            <Footer />
-        </>
+        <main className="flex flex-col gap-[4em] py-[6em] bg-white dark:bg-[#1b1b1b] px-[2em] lg:px-[6em] xl:px-[12em]">
+            <Hero />
+            <Featured />
+            <Benefits />
+        </main>
     );
 };
 
