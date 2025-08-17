effac6c7844290eb7d85f23cf4b9ddda826f5997
shabinx30 <shabeensharih@gmail.com>
2025-08-17 23:45:45 +0530

    refactor: footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,8 +4,29 @@ import { LuFacebook } from "react-icons/lu";
 const Footer = () => {
     return (
         <footer className="bg-[#202020] dark:bg-black p-4">
-            <h3 className="text-xl text-white my-[2em]">Logo</h3>
-            <div className="flex justify-between items-center">
+            <div className="flex py-[2em]">
+                <div className="flex flex-1 justify-center">
+                    <h3 className="text-xl text-white my-[2em] font-bold">Logo</h3>
+                </div>
+                <div className="flex-1/2 flex gap-4 text-sm justify-around items-center">
+                    <div className="flex flex-col gap-2">
+                        <p>About Us</p>
+                        <p>Signin</p>
+                        <p>SignUp</p>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <p>Search for influencers</p>
+                        <p>Search for brands by category</p>
+                        <p>Search for brands</p>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <p>Search for influencers</p>
+                        <p>Search for brands by category</p>
+                        <p>Search for brands</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center gap-4 items-center">
                 <p className="text-sm text-gray-400">
                     All right reserved &copy; 2025
                 </p>
