35a000edd3b8114495264f3b59ada21bc6bd5d79
shabinx30 <shabeensharih@gmail.com>
2025-08-22 22:34:56 +0530

    feat(frontend): sign in with google

--- frontend/src/components/auth/LoginWith.tsx
+++ frontend/src/components/auth/LoginWith.tsx
@@ -1,16 +1,27 @@
 import React from "react";
-import toast from "react-hot-toast";
-import { FaFacebookF, FaGoogle } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa6";
+import { useGoogleLogin } from "@react-oauth/google";
+import { signInWithGoogle } from "@/services";
 
 const LoginWith = () => {
+    const login = useGoogleLogin({
+        onSuccess: async ({ access_token }) => {
+            console.log(access_token);
+            await signInWithGoogle(access_token);
+        },
+        onError: () => console.log("login has been failed"),
+    });
+
     return (
         <section className="flex gap-2">
             <div
-                className="flex flex-1 justify-center p-3 bg-[#3b3b3b] rounded-xl"
+                onClick={() => login()}
+                className="flex flex-1 justify-center p-3 bg-[#3b3b3b] rounded-xl cursor-pointer"
             >
                 <FaGoogle />
             </div>
-            <div className="flex flex-1 justify-center p-3 bg-[#3b3b3b] rounded-xl">
+            <div className="flex flex-1 justify-center p-3 bg-[#3b3b3b] rounded-xl cursor-pointer">
                 <FaFacebookF />
             </div>
         </section>
