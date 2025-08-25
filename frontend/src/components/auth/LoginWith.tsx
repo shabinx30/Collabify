50b59347e7df26ba28d88d27ac18fe7b5e86b097
shabinx30 <shabeensharih@gmail.com>
2025-08-26 00:14:01 +0530

    fix: removed next-themes for better optimized theme switching

--- frontend/src/components/auth/LoginWith.tsx
+++ frontend/src/components/auth/LoginWith.tsx
@@ -7,13 +7,10 @@ import { signInWith } from "@/redux/slices/auth.slice";
 import { AppDispatch } from "@/redux/store/store";
 import { useRouter } from "next/navigation";
 import { jwtDecode } from "jwt-decode";
-import { useTheme } from "next-themes";
 
 const LoginWith = ({ role }: { role?: RoleType }) => {
     const dispatch = useDispatch<AppDispatch>();
     const router = useRouter();
-    const { systemTheme } = useTheme();
-    console.log(systemTheme);
 
     const handleLogin = (credentialResponse: CredentialResponse) => {
         if (credentialResponse && credentialResponse.credential) {
@@ -29,15 +26,18 @@ const LoginWith = ({ role }: { role?: RoleType }) => {
 
     return (
         <section className="flex gap-2">
-            <div className="rounded-full bg-[#000] flex flex-1 justify-center">
-                <GoogleLogin
-                    shape="pill"
-                    theme={systemTheme === "dark" ? "filled_black" : "outline"}
-                    size="large"
-                    onSuccess={handleLogin}
-                    onError={() => console.log("failed")}
-                />
-            </div>
+            <GoogleLogin
+                shape="pill"
+                theme={
+                    window.matchMedia &&
+                    window.matchMedia("(prefers-color-scheme: dark)").matches
+                        ? "filled_black"
+                        : "outline"
+                }
+                size="large"
+                onSuccess={handleLogin}
+                onError={() => console.log("failed")}
+            />
             <div className="flex flex-1 justify-center items-center min-h-full bg-gray-200 dark:bg-[#3b3b3b] rounded-full cursor-pointer">
                 <FaFacebookF />
             </div>
