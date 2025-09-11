0e373c137b89cb54fbb50da70eeb89490a02f5a9
shabinx30 <shabeensharih@gmail.com>
2025-09-11 21:02:26 +0530

    feat: profile dashboard added

--- frontend/src/app/(root)/[userid]/page.tsx
+++ frontend/src/app/(root)/[userid]/page.tsx
@@ -1,32 +1,75 @@
 import { findUser } from "@/services";
-import { IUser } from "@/types/auth/signup.type";
-import { IProfile } from "@/types/profile/profile.type";
+import { IProfile, IProfileUser } from "@/types/profile/profile.type";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
 
 export default async function Profile({ params }: IProfile) {
     const userid = (await params).userid;
-    const data = (await findUser(userid)) as IUser;
+    const data = (await findUser(userid)) as IProfileUser;
 
     if (!data) {
         notFound();
     }
 
+    data.categories.push("content creator");
+    data.categories.push("beauty");
+
     return (
-        <section className="flex justify-center">
-            <div className="flex gap-10 justify-between items-center">
-                <Image
-                    className="w-32 rounded-full border-4 border-[#2b2b2b]"
-                    width={100}
-                    height={100}
-                    src={data.profile}
-                    alt={data.username}
-                />
-                <h3 className="font-semibold text-lg">
-                    {data.username}
-                </h3>
-            </div>
-        </section>
+        <main className="flex gap-4 p-4">
+            <section className="flex-1">
+                <h3 className="text-amber-400">Person Details</h3>
+                <div className="flex bg-[#2b2b2b] rounded-xl p-2">
+                    <div className="relative max-h-fit">
+                        <Image
+                            className="min-w-36 rounded-2xl"
+                            width={100}
+                            height={100}
+                            src={data.profile}
+                            alt={data.username}
+                        />
+                        <div className="absolute bottom-1 left-1 py-2 w-[calc(100%-8px)] bg-[#ffffff80] backdrop-blur-md rounded-full">
+                            <p className="text-center text-black text-sm">
+                                change profile photo
+                            </p>
+                        </div>
+                    </div>
+                    <section className="flex flex-col w-full gap-3 mx-10">
+                        <div className="flex justify-between">
+                            <div className="leading-[1.25]">
+                                <span className="text-gray-400 text-xs">
+                                    Name
+                                </span>
+                                <h3>{data.username}</h3>
+                            </div>
+                            <div className="leading-[1.25]">
+                                <span className="text-gray-400 text-xs">
+                                    Charge
+                                </span>
+                                <h3>{data.amount ? data.amount : 0}$</h3>
+                            </div>
+                        </div>
+                        <div className="leading-[1.25]">
+                            <span className="text-gray-400 text-xs">Email</span>
+                            <h3>{data.email}</h3>
+                        </div>
+                        <div>
+                            <span className="text-gray-400 text-xs">Tags</span>
+                            <div className="flex gap-1 pt-0.5 flex-wrap">
+                                {data.categories.map((category, i) => (
+                                    <p key={i} className="bg-amber-400/5 px-2 pt-0.5 pb-1 text-xs text-amber-400 border border-amber-400 rounded-full max-w-fit">
+                                        {category}
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </section>
+            <section className="flex-1">
+                <h3 className="text-amber-400">Earning</h3>
+                <div className="bg-[#2b2b2b] rounded-xl p-2"></div>
+            </section>
+        </main>
     );
 }
