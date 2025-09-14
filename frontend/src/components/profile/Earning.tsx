4562778ac2de8151bf76b56060e071cabf85e0e8
shabinx30 <shabeensharih@gmail.com>
2025-09-14 23:15:08 +0530

    style(profile): optimized for white mode

--- frontend/src/components/profile/Earning.tsx
+++ frontend/src/components/profile/Earning.tsx
@@ -4,7 +4,7 @@ const Earning = () => {
     return (
         <section className="flex-1">
             <h3 className="text-amber-400">Earning</h3>
-            <div className="bg-[#2b2b2b] rounded-xl p-2"></div>
+            <div className="bg-gray-100 dark:bg-[#2b2b2b] rounded-xl p-2"></div>
         </section>
     );
 };
