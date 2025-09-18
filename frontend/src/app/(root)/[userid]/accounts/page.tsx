67dba74001853b51acca6d36f058c08a11607956
shabinx30 <shabeensharih@gmail.com>
2025-09-18 23:25:56 +0530

    feat: initiated settings and accounts

--- frontend/src/app/(root)/[userid]/accounts/page.tsx
+++ frontend/src/app/(root)/[userid]/accounts/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { FiPlus } from "react-icons/fi";
 
 const Accounts = () => {
-    return <div>Accounts</div>;
+    return (
+        <main className="p-4">
+            <section className="flex-col flex-1">
+                <h3 className="text-amber-400">Accounts</h3>
+                <div className="w-full p-4 bg-[#2b2b2b] rounded-2xl">
+                    <div className="flex flex-col gap-2 items-center py-4">
+                        <p className="text-gray-500 text-sm">
+                            No Accounts haven't added yet!
+                        </p>
+                        <button className="bg-white rounded-2xl text-black py-1 pl-2.5 pr-3.5 flex items-center gap-1">
+                            <FiPlus />
+                            Add an account
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
 };
 
 export default Accounts;
--- frontend/src/app/(root)/[userid]/settings/page.tsx
+++ frontend/src/app/(root)/[userid]/settings/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Settings = () => {
-    return <div>settings</div>;
+    return (
+        <main className="p-4">
+            <section className="flex-col flex-1">
+                <h3 className="text-amber-400">Settings</h3>
+                <div className="p-4 w-full bg-[#2b2b2b] rounded-2xl"></div>
+            </section>
+        </main>
+    );
 };
 
 export default Settings;
