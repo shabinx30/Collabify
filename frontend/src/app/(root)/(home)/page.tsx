f9b65a5c1680ed8aa63954043ed8004a1ab39840
shabinx30 <shabeensharih@gmail.com>
2025-10-25 20:15:29 +0530

    refine(animation): home & search

--- frontend/src/app/(root)/(home)/page.tsx
+++ frontend/src/app/(root)/(home)/page.tsx
@@ -1,13 +1,13 @@
 import Benefits from "@/components/home/Benefits";
 import Featured from "@/components/home/Featured";
 import Hero from "@/components/home/Hero";
-import "../../../components/home/home.css"
+import "../../../components/home/home.css";
 
 const Home = () => {
     return (
         <main className="flex flex-col gap-[4em] py-[4em] bg-white dark:bg-[#1b1b1b] px-[2em] lg:px-[6em] xl:px-[12em]">
             <Hero />
-            <Featured />
+            <Featured header={true} />
             <Benefits />
         </main>
     );
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -17,16 +17,20 @@ import {
     DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { logoutUser } from "@/redux/slices/auth.slice";
-import { ViewTransition } from "react";
+import { FormEvent, ViewTransition } from "react";
 
 const NavBar = () => {
     const pathnames = usePathname().split("/");
-    const path = pathnames[0];
+    const path = pathnames[1];
     const { user } = useSelector((state: RootState) => state.auth);
     const dispatch = useDispatch<AppDispatch>();
     const router = useRouter();
 
     const handleLogout = () => dispatch(logoutUser(router));
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+    };
 
     return (
         <nav className={"bg-white dark:bg-[#1b1b1b]"}>
@@ -41,77 +45,85 @@ const NavBar = () => {
                         className="hidden md:flex flex-1 items-center px-4 gap-2 border border-[#868686] dark:border-gray-600 bg-gray-100 dark:bg-[#2b2b2b] rounded-xl"
                     >
                         <FiSearch />
-                        <input
-                            type="text"
-                            className="w-full h-10 outline-none"
-                            placeholder="Search"
-                        />
+                        <form onSubmit={handleSubmit} autoComplete="off">
+                            <input
+                                type="search"
+                                name="search"
+                                className="w-full h-10 outline-none"
+                                placeholder="Search"
+                            />
+                        </form>
                     </li>
                 </ViewTransition>
                 <li className="flex-1/3 lg:flex-1 flex text-sm items-center justify-end gap-6">
                     {user ? (
-                        <DropdownMenu>
-                            <DropdownMenuTrigger
-                                asChild
-                                className="border-none"
-                            >
-                                <div className="flex gap-4 items-center hover:bg-gray-200 dark:hover:bg-[#3b3b3b] duration-200 pl-4 rounded-full cursor-default">
-                                    <h3>{user.username}</h3>
-                                    <Image
-                                        className={`w-9 h-9 rounded-full ${
-                                            !user.profile
-                                                ? "contrast-0 dark:contrast-100"
-                                                : ""
-                                        }`}
-                                        width={100}
-                                        height={100}
-                                        src={
-                                            user.profile
-                                                ? user.profile
-                                                : "/images/icons/user.png"
-                                        }
-                                        alt={user.username}
-                                    />
-                                </div>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent
-                                className="w-56"
-                                align="center"
-                            >
-                                <DropdownMenuLabel>
-                                    My Account
-                                </DropdownMenuLabel>
-                                <DropdownMenuGroup>
-                                    <Link href={`/${user.username}`}>
-                                        <DropdownMenuItem>
-                                            Profile
-                                            <DropdownMenuShortcut>
-                                                ⇧⌘P
-                                            </DropdownMenuShortcut>
-                                        </DropdownMenuItem>
-                                    </Link>
-                                    <Link href={`/${user.username}/settings`}>
-                                        <DropdownMenuItem>
-                                            Settings
-                                            <DropdownMenuShortcut>
-                                                ⌘S
-                                            </DropdownMenuShortcut>
-                                        </DropdownMenuItem>
-                                    </Link>
-                                </DropdownMenuGroup>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem
-                                    onClick={handleLogout}
-                                    onMouseEnter={(e) =>
-                                        (e.currentTarget.style.color =
-                                            "#fb2c36")
-                                    }
-                                    className="text-red-500"
+                        path !== user.username && (
+                            <DropdownMenu>
+                                <DropdownMenuTrigger
+                                    asChild
+                                    className="border-none"
                                 >
-                                    Log out
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
+                                    <div className="flex gap-4 items-center hover:bg-gray-200 dark:hover:bg-[#3b3b3b] duration-200 pl-4 rounded-full cursor-default">
+                                        <h3>{user.username}</h3>
+                                        <ViewTransition name="profile">
+                                            <Image
+                                                className={`w-9 h-9 rounded-full ${
+                                                    !user.profile
+                                                        ? "contrast-0 dark:contrast-100"
+                                                        : ""
+                                                }`}
+                                                width={100}
+                                                height={100}
+                                                src={
+                                                    user.profile ||
+                                                    "/images/icons/user.png"
+                                                }
+                                                alt={user.username}
+                                            />
+                                        </ViewTransition>
+                                    </div>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent
+                                    className="w-56"
+                                    align="center"
+                                >
+                                    <DropdownMenuLabel>
+                                        My Account
+                                    </DropdownMenuLabel>
+                                    <DropdownMenuGroup>
+                                        <Link href={`/${user.username}`}>
+                                            <DropdownMenuItem>
+                                                Profile
+                                                <DropdownMenuShortcut>
+                                                    ⇧⌘P
+                                                </DropdownMenuShortcut>
+                                            </DropdownMenuItem>
+                                        </Link>
+                                        <Link
+                                            href={`/${user.username}/settings`}
+                                        >
+                                            <DropdownMenuItem>
+                                                Settings
+                                                <DropdownMenuShortcut>
+                                                    ⌘S
+                                                </DropdownMenuShortcut>
+                                            </DropdownMenuItem>
+                                        </Link>
+                                    </DropdownMenuGroup>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuItem
+                                        onClick={handleLogout}
+                                        onMouseEnter={(e) =>
+                                            (e.currentTarget.style.color =
+                                                "#fb2c36")
+                                        }
+                                        className="text-red-500"
+                                    >
+                                        Log out
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        )
                     ) : !path ? (
                         <>
                             <Link href="/signin">Login</Link>{" "}
