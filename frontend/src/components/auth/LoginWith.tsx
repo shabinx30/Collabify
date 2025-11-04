a6a8934255bb9b4453cc72f3903f1cabcf8a81db
shabinx30 <shabeensharih@gmail.com>
2025-11-04 15:18:16 +0530

    feat: instagram auth

--- frontend/src/components/auth/LoginWith.tsx
+++ frontend/src/components/auth/LoginWith.tsx
@@ -1,4 +1,3 @@
-import { FaFacebookF } from "react-icons/fa";
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import { IDecode, RoleType } from "@/types/auth/signup.type";
 import { useDispatch } from "react-redux";
@@ -6,6 +5,8 @@ import { signInWith } from "@/redux/slices/auth.slice";
 import { AppDispatch } from "@/redux/store/store";
 import { useRouter } from "next/navigation";
 import { jwtDecode } from "jwt-decode";
+import { SiInstagram } from "react-icons/si";
+import axios from "axios";
 
 const LoginWith = ({ role }: { role?: RoleType }) => {
     const dispatch = useDispatch<AppDispatch>();
@@ -23,6 +24,13 @@ const LoginWith = ({ role }: { role?: RoleType }) => {
         }
     };
 
+    const handleInsta = async () => {
+        const response = await axios.post(
+            "https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=1099021975409872&redirect_uri=https://unknown-shabin.vercel.app/&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights"
+        );
+        console.log({ insta: response.data });
+    };
+
     return (
         <section className="flex gap-2">
             <GoogleLogin
@@ -37,8 +45,12 @@ const LoginWith = ({ role }: { role?: RoleType }) => {
                 onSuccess={handleLogin}
                 onError={() => console.log("failed")}
             />
-            <div className="flex flex-1 justify-center items-center min-h-full bg-gray-200 dark:bg-[#3b3b3b] rounded-full cursor-pointer">
-                <FaFacebookF />
+            <div
+                onClick={handleInsta}
+                className="flex flex-1 justify-center items-center gap-2 min-h-full bg-gray-200 dark:bg-[#3b3b3b] rounded-full cursor-pointer"
+            >
+                <SiInstagram size={18} />
+                <p>Link with Instagram</p>
             </div>
         </section>
     );
