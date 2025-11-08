e3f8da83d140d9879545bc7e07c07e1f5085f0f8
shabinx30 <shabeensharih@gmail.com>
2025-11-08 22:50:51 +0530

    fix: saving social media doc

--- frontend/src/app/api/auth/instagram/callback/route.ts
+++ frontend/src/app/api/auth/instagram/callback/route.ts
@@ -56,6 +56,8 @@ export async function GET(request: Request) {
             },
         });
 
+        media.save()
+
         const res = NextResponse.redirect(
             "https://collabify-shabin.vercel.app/dashboard"
         );
