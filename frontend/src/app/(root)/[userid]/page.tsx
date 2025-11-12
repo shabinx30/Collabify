d2d238a1cb03f5a0a579d67cffdbd780b5901749
shabinx30 <shabeensharih@gmail.com>
2025-11-12 23:59:01 +0530

    feat: logout

--- frontend/src/app/(root)/[userid]/page.tsx
+++ frontend/src/app/(root)/[userid]/page.tsx
@@ -3,6 +3,7 @@ import PersonalDetails from "@/components/profile/PersonalDetails";
 import { findUser } from "@/services";
 import { IProfile, IProfileUser } from "@/types/profile/profile.type";
 import { notFound } from "next/navigation";
+import { TbLogout } from "react-icons/tb";
 
 export default async function Profile({ params }: IProfile) {
     const userid = (await params).userid;
@@ -16,6 +17,7 @@ export default async function Profile({ params }: IProfile) {
         <main className="flex flex-col md:flex-row flex-1 gap-4 p-2 sm:p-4">
             <PersonalDetails data={data} />
             <Earning />
+            <section className="flex justify-center my-8"><div className="flex gap-1 items-center md:hidden text-red-400 bg-gray-100 dark:bg-black rounded-2xl px-4 pt-1.5 py-2"><TbLogout size={18} /><p>Logout</p></div></section>
         </main>
     );
 }
