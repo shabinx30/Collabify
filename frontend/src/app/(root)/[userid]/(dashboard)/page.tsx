17287ab947e421f6b6c9cfffb15a2a8cb6f79ac5
shabinx30 <shabeensharih@gmail.com>
2025-11-16 00:43:29 +0530

    refactor: parallel route for profile cmp

--- frontend/src/app/(root)/[userid]/(dashboard)/page.tsx
+++ frontend/src/app/(root)/[userid]/(dashboard)/page.tsx
@@ -1,21 +1,5 @@
-import Earning from "@/components/profile/Earning";
-import Logout from "@/components/profile/Logout";
-import { findUser } from "@/services";
-import { IProfile, IProfileUser } from "@/types/profile/profile.type";
-import { notFound } from "next/navigation";
+const Page = () => {
+    return null
+};
 
-export default async function Profile({ params }: IProfile) {
-    const userid = (await params).userid;
-    const userData = (await findUser(userid)) as IProfileUser;
-
-    if (!userData) {
-        notFound();
-    }
-
-    return (
-        <main className="flex flex-col md:flex-row flex-1 gap-4 p-2 sm:p-4">
-            <Earning />
-            <Logout />
-        </main>
-    );
-}
+export default Page;
--- frontend/src/contexts/UserDataContext.tsx
+++ frontend/src/contexts/UserDataContext.tsx
@@ -26,4 +26,3 @@ export const UserDataProvider = ({
         </UserDataContext.Provider>
     );
 };
-
