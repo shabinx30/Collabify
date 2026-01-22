a9834b8a5219262f7990f3dd2f80d7d7d71a383b
shabinx30 <shabeensharih@gmail.com>
2026-01-22 22:36:39 +0530

    feat(home)!: caching for featured creators

--- frontend/src/components/home/Featured.tsx
+++ frontend/src/components/home/Featured.tsx
@@ -1,25 +1,20 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import Tiles from "../ui/Tiles";
-import { searchCreators } from "@/services";
+import { IProfileUser } from "@/types/profile/profile.type";
 
-const Featured = () => {
-    const [creators, setCreators] = useState([]);
-
-    useEffect(() => {
-        (async () => {
-            const response = await searchCreators();
-            setCreators(response);
-        })();
-    }, []);
+const Featured = async () => {
+    const creators = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/creators`,
+        {
+            next: { revalidate: 60 },
+        },
+    ).then((res) => res.json());
 
     return (
         <section>
             <h1 className="font-semibold text-xl">Featured</h1>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 py-[1em]">
                 {creators && creators.length ? (
-                    creators.map((creator, i) => (
+                    creators.map((creator: IProfileUser, i: number) => (
                         <Tiles key={i} creator={creator} />
                     ))
                 ) : (
