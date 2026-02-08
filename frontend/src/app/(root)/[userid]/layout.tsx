aa2920948e36820f4130877aaaa9126367267900
shabinx30 <shabeensharih@gmail.com>
2026-02-08 20:10:03 +0530

    feat: myprofile vs others profile

--- frontend/src/app/(root)/[userid]/layout.tsx
+++ frontend/src/app/(root)/[userid]/layout.tsx
@@ -1,12 +1,31 @@
 import NavBar from "@/components/NavBar";
 import Aside from "@/components/profile/Aside";
+import { UserDataProvider } from "@/contexts/UserDataContext";
+import { findUser } from "@/services";
+import { IProfileUser } from "@/types/profile/profile.type";
+import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({
+    children,
+    params,
+}: {
+    children: React.ReactNode;
+    params: Promise<{ userid: string }>;
+}) => {
+    const userid = (await params).userid;
+    const userData = (await findUser(userid)) as IProfileUser;
+
+    if (!userData) {
+        notFound();
+    }
+
     return (
         <Suspense>
-            <NavBar />
-            <Aside>{children}</Aside>
+            <UserDataProvider userData={userData}>
+                <NavBar />
+                <Aside>{children}</Aside>
+            </UserDataProvider>
         </Suspense>
     );
 };
