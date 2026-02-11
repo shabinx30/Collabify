ca7b8f65d2440e8d2a2e4f648ccddf5ad9864251
shabinx30 <shabeensharih@gmail.com>
2026-02-11 23:56:01 +0530

    refine: animation

--- frontend/src/components/ui/Tiles.tsx
+++ frontend/src/components/ui/Tiles.tsx
@@ -1,17 +1,20 @@
 import { IProfileUser } from "@/types/profile/profile.type";
 import Image from "next/image";
+import { ViewTransition } from "react";
 
 const Tiles = ({ creator }: { creator: IProfileUser }) => {
     return (
         <div className="aspect-9/14">
             <div className="relative h-[84%] w-full">
-                <Image
-                    className="object-cover w-full h-full rounded-xl"
-                    src={creator.profile ?? "/images/infu-1.jpeg"}
-                    alt="infu"
-                    fill
-                    sizes="(max-width: 768px) 100vw, 50vw"
-                />
+                <ViewTransition name={creator._id}>
+                    <Image
+                        className="object-cover w-full h-full rounded-xl"
+                        src={creator.profile ?? "/images/infu-1.jpeg"}
+                        alt="infu"
+                        fill
+                        sizes="(max-width: 768px) 100vw, 50vw"
+                    />
+                </ViewTransition>
             </div>
             <div className="flex justify-between text-[0.9rem]">
                 <div>
