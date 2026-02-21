97327e1990496d2cf4302d9d3c77ecf7e9df1a57
shabinx30 <shabeensharih@gmail.com>
2026-02-21 23:32:36 +0530

    refine: saved prfl in prfl page

--- frontend/src/app/(root)/[userid]/saved-profiles/page.tsx
+++ frontend/src/app/(root)/[userid]/saved-profiles/page.tsx
@@ -3,7 +3,11 @@ const SavedProfiles = () => {
         <main className="p-4">
             <section className="flex-col flex-1">
                 <h3 className="font-semibold dark:text-base mb-1">Saved Profiles</h3>
-                <div className="p-4 w-full bg-gray-100 dark:bg-[#2b2b2b] rounded-2xl"></div>
+                <div className="py-8 w-full bg-gray-100 dark:bg-[#2b2b2b] rounded-2xl">
+                    <p className="text-center text-sm text-gray-500 dark:text-gray-400">
+                        No Saved Profiles
+                    </p>
+                </div>
             </section>
         </main>
     );
