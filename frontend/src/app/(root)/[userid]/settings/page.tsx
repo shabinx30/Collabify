2914d1aa2220bcb8c3f881f6d22b114d8c739397
shabinx30 <shabeensharih@gmail.com>
2026-02-22 23:35:49 +0530

    refine: settings page

--- frontend/src/app/(root)/[userid]/settings/page.tsx
+++ frontend/src/app/(root)/[userid]/settings/page.tsx
@@ -3,7 +3,11 @@ const Settings = () => {
         <main className="p-4">
             <section className="flex-col flex-1">
                 <h3 className="font-semibold dark:text-base mb-1">Settings</h3>
-                <div className="p-4 w-full bg-gray-100 dark:bg-[#2b2b2b] rounded-2xl"></div>
+                <div className="py-8 w-full bg-gray-100 dark:bg-[#2b2b2b] rounded-2xl">
+                    <p className="text-center text-sm text-gray-500 dark:text-gray-400">
+                        No settings available yet.
+                    </p>
+                </div>
             </section>
         </main>
     );
